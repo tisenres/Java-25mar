bdc35dedb868fdf21423f23954c3306219340226
tisenres <tisenres@gmail.com>
2024-03-25 12:21:30 +0500

    t

--- src/main/kotlin/cyber/Interval.kt
+++
@@ -1,9 +0,0 @@
-package cyber
-
-fun main(args: Array<String>) {
-    val str = "A-Y"
-    val range = str.split("-")
-    for (i in range[0].first().code..range[1].first().code) {
-        print(Char(i) + " ")
-    }
-}
